a5adb9138184b7e3c63672d7d8c57cf2ed8ec56c
ClassicBrend <br_man_05@yahoo.co.uk>
2014-02-25 14:48:45 +0000

    Update models.py

--- workspace/go_scavenge_project/game/models.py
+++ workspace/go_scavenge_project/game/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.OneToOneField(blank=full, Primary_Key=True)
+    username = models.OneToOneField(blank=true, Primary_Key=True)
     def __unicode__(self):
         return self.username
         
