71064d9b7ee0e135bbb54214e20b9d85d5603692
ClassicBrend <br_man_05@yahoo.co.uk>
2014-02-25 14:08:31 +0000

    Update models.py

--- workspace/go_scavenge_project/game/models.py
+++ workspace/go_scavenge_project/game/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=20)
-    
+class UserProfile(models.Model):
+    username = models.OneToOneField(blank=full, Primary_Key=True)
     def __unicode__(self):
         return self.username
+    
+class Game(models.Model):
+    gameID = models.IntegerField(Primary_Key=True)
+    score = models.IntegerField()
+
+    
+class Leaderboard(models.Model):
+    name = models.ForeignKey(UserProfile)
+    gameID = models.ForeignKey(Game)
+    score = models.IntegerField()
+    
+class Supplies(models.Model):
+    supplyName = models.CharField(max_length=60)
+    supplyType = models.CharField(max_length=60)
+    
+    
+    
+    
         
 
