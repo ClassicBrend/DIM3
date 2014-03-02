99f07a173afa7337895baebe41b8e5e4da575f2c
ClassicBrend <br_man_05@yahoo.co.uk>
2014-03-02 16:08:00 +0000

    Updated Models.py to new correct format

--- workspace/go_scavenge_project/game/models.py
+++ workspace/go_scavenge_project/game/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.OneToOneField(blank=True, Primary_Key=True)
-    def __unicode__(self):
-        return self.username
+    username = models.CharField(max_length=30, primary_key=true)
+    password = models.CharField(max_length=30)
+
+class Stats(models.Model):
+    user = models.ForeignKey(User)
+    longest_session = models.CharField(max_length=30)
+    max_survivors = models.CharField(max_length=30)
         
 
 
