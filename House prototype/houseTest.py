a6b5b76defe096887b9d9d8b2c6082061db7c293
DrouinD <2036584d@student.gla.ac.uk>
2014-03-10 23:47:35 +0000

    Added more functionality with proper constructors

--- House prototype/houseTest.py
+++ House prototype/houseTest.py
@@ -11,31 +11,74 @@ def determineOutcome(food,ammo,zombies,surv):
     player.ammo += ammo
     player.food += food
     player.survivors += surv
-    
-    
+    player.moves -= 1
+
+def newarea():
+	global houses
+	houses=[]
+	house1=House()
+	houses.append(house1)
+	house2=House()
+	houses.append(house2)
+	house3=House()
+	houses.append(house3)
+	house4=House()
+	houses.append(house4)
+	house5=House()
+	houses.append(house5)	
+	house6=House()
+	houses.append(house6)	
+	
+def rummage(houseNum):
+	if player.moves > 0:
+		house=houses[houseChoice]
+		room = dict[houseNum]
+	
+		print "player enters house ", houseNum, " which has ", house.roomsInHouse, " rooms"
+		print "survivors: ", player.survivors
+		print "food: ", player.food
+		print "ammo: ", player.ammo
+		print "ROOM = ", room + 1, " HOUSE = ", houseNum
+		determineOutcome(house.foodInHouse[room],house.ammoInHouse[room],house.zombiesInHouse[room],house.survivorsInHouse[room])
+	
+		print "player leaves room ", room + 1, " with"
+		print "survivors: ", player.survivors
+		print "food: ", player.food
+		print "ammo: ", player.ammo
+		print "moves left: ", player.moves
+		print "---------------------------------"
+		room += 1
+		dict[houseNum] = room
+	else:
+		print "No more moves"
+	
 class House():
-    roomsInHouse = randint(1,3)
-    foodInHouse = []
-    ammoInHouse = []
-    zombiesInHouse = []
-    survivorsInHouse = []
-    for i in range(0,roomsInHouse):
-        foodInHouse.append(randint(1,10))
-        ammoInHouse.append(randint(0,10))
-        zombiesInHouse.append(randint(0,5))
-        survivorsInHouse.append(randint(0,3))
-        
+
+	def __init__(self):
+	    self.roomsInHouse = randint(1,3)
+	    self.foodInHouse = []
+	    self.ammoInHouse = []
+	    self.zombiesInHouse = []
+	    self.survivorsInHouse = []
+	    
+	    for i in range(0,self.roomsInHouse):
+			self.foodInHouse.append(randint(1,10))
+			self.ammoInHouse.append(randint(0,10))
+			self.zombiesInHouse.append(randint(0,5))
+			self.survivorsInHouse.append(randint(0,3))
+
 class Player():
-    survivors = 3
-    food = 0
-    ammo = 0
 
-    
+	def __init__(self):
+	    self.survivors=3
+	    self.food = 0
+	    self.ammo = 0
+	    self.moves = 8
 
-house = House()
 player = Player()
-
-
+newarea()
+houseChoice = 0
+dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
 print "|||||||||||||||||||"
 print "| House Generator |"
 print "|||||||||||||||||||"
@@ -44,30 +87,35 @@ print "player stats"
 print "survivors: ", player.survivors
 print "food: ", player.food
 print "ammo", player.ammo
+print "moves: ", player.moves
 print ""
-print "There are ", house.roomsInHouse, " rooms in this house"
+print "There are ", len(houses)-1, " houses"
 print ""
 
-for i in range(0,house.roomsInHouse):
-    print "player enters room ",i," with "
-    print "survivors: ", player.survivors
-    print "food: ", player.food
-    print "ammo: ", player.ammo
-    print ""
+while player.survivors>0:
+	if player.moves < 8:
+		a=raw_input("Would you like to move to another house y/n:")
+		if a=="n":
+			if house.roomsInHouse == dict[houseChoice]:
+				print "The house is empty, pick another!"
+			else:
+				rummage(houseChoice)
+				continue
+		else:
+			player.moves -= 1
 
-    print "Room number ", i
-    print "food: ", house.foodInHouse[i]
-    print "ammo: ", house.ammoInHouse[i]
-    print "zomb: ", house.zombiesInHouse[i]
-    print "surv: ", house.survivorsInHouse[i]
-    print ""
-    print ""
-    determineOutcome(house.foodInHouse[i],house.ammoInHouse[i],house.zombiesInHouse[i],house.survivorsInHouse[i])
-    print "player leaves room ",i," with "
-    print "survivors: ", player.survivors
-    print "food: ", player.food
-    print "ammo: ", player.ammo
-    print "---------------------------------"
-    
-    
-    
+
+	if houseChoice != 0:
+		previousChoice = houseChoice
+	else:
+		previousChoice = houseChoice
+	if player.moves > 0:
+		houseChoice = input("Pick a house: ")
+		house = houses[houseChoice]
+		if house.roomsInHouse == dict[houseChoice]:
+			print "The house is empty, pick another!"
+		else:
+			rummage(houseChoice)
+			
+print "GAME OVER"
+
