6cfa3bdc6d369d4fc96a276c14fb4cd0e8fb1291
smithand <andrew_m_smith@hotmail.co.uk>
2014-03-11 12:46:20 +0000

    Updated css files, and profile page

--- workspace/go_scavenge_project/game/forms.py
+++ workspace/go_scavenge_project/game/forms.py
@@ -4,13 +4,19 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
-        
+        fields = ['username', 'email', 'password']
+
+
 class UserProfileForm(forms.ModelForm):
+    picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
+    about = forms.URLField(help_text="About yourself", required=False)
+
     class Meta:
         model = UserProfile
-        fields = ('picture','about')
+        fields = ('picture', 'about')
